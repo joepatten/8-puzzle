2bf45e9535b3e7173f01bb644175fae7b5d61092
joepatten <joseph.patten@wsu.edu>
2019-09-10 12:16:02 -0700

    seperated game and puzzle board into seperate files

---
+++ game.py
@@ -0,0 +1,54 @@
+import os
+import curses
+
+
+os.chdir(r'C:\Users\josep\python_files\CptS_540\8_puzzle')
+from puzzle_board import Puzzle
+
+
+def play_game():
+    screen = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+    screen.keypad(True)
+    screen.addstr("Welcome to the 8 puzzle game.\n")
+    p = Puzzle()
+    screen.addstr("Press any button to begin the game.\n")
+    screen.getch()
+
+    moves = 0
+    while True:
+        screen.clear()
+        screen.addstr('Use the arrow keys to move the blank square. Press Esc or e to exit.\n')
+        screen.addstr(p.board_string())
+        if p.check_board():
+            screen.addstr(f'You won in {moves} moves. Hooray.\nPress any key to exit.')
+            screen.getch()
+            break
+        c = screen.getch()
+        if c == ord('e') or c == 27:
+            break
+        elif c == ord('w') or c == 259:
+            p.move('up')
+        elif c == ord('s') or c == 258:
+            p.move('down') 
+        elif c == ord('a') or c == 260:
+            p.move('left')
+        elif c == ord('d') or c == 261:
+            p.move('right')
+        else:
+            continue
+        moves += 1
+        screen.refresh()
+
+
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(1)
+    screen.keypad(False)
+    curses.endwin()
+
+
+if __name__ == "__main__":
+    play_game()
--- puzzle_board.py
+++ puzzle_board.py
@@ -1,9 +1,7 @@
 import numpy as np
-import subprocess as sp
-import curses
 import random
 
-class puzzle():
+class Puzzle():
     def __init__(self, board=None):
         self.goal_board = np.array([[1,2,3],[4,5,6],[7,8,' ']])
         if board == None:
@@ -48,48 +46,3 @@ class puzzle():
         for i in range(50):
             direction = random.choice(list(self.d.keys()))
             self.move(direction)
-        
-    
-if __name__ == "__main__":
-    screen = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(0)
-    screen.keypad(True)
-    screen.addstr("Welcome to the 8 puzzle game.\n")
-    p = puzzle()
-    screen.addstr("Press any button to begin the game.\n")
-    screen.getch()
-    
-    moves = 0
-    while True:
-        screen.clear()
-        screen.addstr('Use the arrow keys to move the blank square. Press Esc or e to exit.\n')
-        screen.addstr(p.board_string())
-        if p.check_board():
-            screen.addstr(f'You won in {moves} moves. Hooray.\nPress any key to exit.')
-            screen.getch()
-            break
-        c = screen.getch()
-        if c == ord('e') or c == 27:
-            break
-        elif c == ord('w') or c == 259:
-            p.move('up')
-        elif c == ord('s') or c == 258:
-            p.move('down') 
-        elif c == ord('a') or c == 260:
-            p.move('left')
-        elif c == ord('d') or c == 261:
-            p.move('right')
-        else:
-            continue
-        moves += 1
-        screen.refresh()
-        
-        
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(1)
-    screen.keypad(False)
-    curses.endwin()
-        
